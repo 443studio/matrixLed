b1aaaef84926c8316dd41f0f0d0f544c05be4e3b
443studio <yoshimi1039@gmail.com>
2016-10-29 09:57:25 +0900

    修正 実働確認

--- matrixLedby74hc595.py
+++ matrixLedby74hc595.py
@@ -1,89 +1,93 @@
 # -*- coding: utf-8 -*-
 #control matrixLed 16x16 by Shift Resister(74hc595)
-#2016/10/28 Ver3.01
+#2016/10/29 Ver3.1
 
 import RPi.GPIO as GPIO
 import time
 import threading
 
-Ct_SCK = 1
-Ct_RCK = 2
-Ct_SI  = 3
-An_SCK = 4
-An_RCK = 5
-An_SI  = 6
+
 loop_flag = True
 
 def setup():
     GPIO.setmode(GPIO.BCM)
-    for i in range(1,7):
+    GPIO.setwarnings(False)
+    for i in range(2,8):
         GPIO.setup(i, GPIO.OUT)
         GPIO.output(i,GPIO.LOW)
 
 class matrixSResister:
     def __init__(self):
-        self.drawList = [[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],]
+        self.Ct = [2,3,4]
+        self.An = [5,6,7]
+        self.SCK = 0
+        self.RCK = 1
+        self.SI = 2     #zanteitekinataisyo
+        self.loop_flag = True
+        self.drawList =
+[[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],]
 
-    def shift(self, PIN):
+    def shift(self, PIN,PIN2):
         GPIO.output(PIN,GPIO.HIGH)
         GPIO.output(PIN,GPIO.LOW)
-    
+
     def send(self, data, axis):
-        pin = lambda a, b: a + "_" + b
-        for i in range(16):
-            if ((1 << i ) & data) == 0:
-                GPIO.output(pin(axis, "SI"), GPIO.LOW)
+        for i in range(0,16):
+            if ((1<<i)&data) == 0:
+                GPIO.output(axis[self.SI], GPIO.LOW)
             else:
-                GPIO.output(pin(axis, "SI"), GPIO.HIGH)
-            shift(pin(axis, "SCK"))
+                GPIO.output(axis[self.SI], GPIO.HIGH)
+            self.shift(axis[self.SCK])
 
     def reflect(self):
-        shift(An_RCK)
-        shift(Ct_RCK)
+        self.shift(self.Ct[self.RCK])
+        self.shift(self.An[self.RCK])
 
-    def flashLed(self,name):
-        ct = 65534
+    def flashLed(self):
+        ctptn = 65534
         for x in range(0,16):
-            send(ct,"Ct")
-            send(self.drawList[x],"An")
-            reflect()
+            self.send(ctptn,self.Ct)
+            self.send(self.drawList[x],self.An)
+            self.reflect()
             time.sleep(0.000001)
-            ct = ((ct-32768) << 1) +1
-        if loop_flag:
-            threading.Thread(target= self.flashLed)
+            ctptn = ((ctptn-32768) << 1) +1
+        if self.loop_flag == True:
+            thr = threading.Thread(target= self.flashLed)
+            thr.start()
         else:
-            send_bits(0)
-            reflect()
-            
+            self.send(0,self.Ct)
+            self.reflect()
+
     def pushList(self,newList):
         self.drawList = newList
 
-dummyList = [[21845],
-             [43690],
-             [21845],
-             [43690],
-             [21845],
-             [43690],
-             [21845],
-             [43690],
-             [21845],
-             [43690],
-             [21845],
-             [43690],
-             [21845],
-             [43690],
-             [21845],
-             [43690],]
+
+dummyList = [0b0,
+             0b0,
+              0b11111111111110,
+                        0b1000,
+                       0b10000,
+                      0b100000,
+                   0b101000000,
+                   0b110000000,
+                   0b100000000,
+                   0b100000000,
+                   0b100000000,
+                  0b1000000000,
+                  0b1000000000,
+                 0b10000000000,
+               0b1100000000000,
+             0b0,]
 
 try:
     setup()
-    mtrLed = matrixSResiter()
+    mtrLed = matrixSResister()
     mtrLed.pushList(dummyList)
     mtrLed.flashLed()
     while True:
-        sleep(0.5)
+        time.sleep(0.5)
 
 except KeyboardInterrupt:
-    loop_flag = False
-    time.sleep(0.1)
+    mtrLed.loop_flag = False
+    time.sleep(5)
     GPIO.cleanup()
